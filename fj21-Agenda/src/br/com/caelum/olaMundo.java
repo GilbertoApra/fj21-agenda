e843467a23b9964aa8f75f321c2e26102a8d96f9
web6551 <web6551@caelum144-17>
2017-06-17 11:55:50 -0300

    Web Dynamic

--- fj21-Agenda/src/br/com/caelum/olaMundo.java
+++ fj21-Agenda/src/br/com/caelum/olaMundo.java
@@ -1,5 +1,29 @@
 package br.com.caelum;
 
-public class olaMundo {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/oi")
+public class olaMundo extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1> Ola Mundo!");
+		out.println("</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
 
 }
