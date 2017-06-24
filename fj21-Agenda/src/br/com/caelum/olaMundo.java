4c40d8e460d775ce1d25a2cea76b0d3d623467eb
web6551 <web6551@caelum144-17>
2017-06-24 09:21:32 -0300

    sa

--- fj21-Agenda/src/br/com/caelum/olaMundo.java
+++ fj21-Agenda/src/br/com/caelum/olaMundo.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/oi")
+@WebServlet(name="serv", urlPatterns={"/oi","/ola"})
 public class olaMundo extends HttpServlet{
 	
 	@Override
